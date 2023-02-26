d90fd6ae01b384976b0998eea0dc575bbd848ae5
Amelia Trevino <81050391+Mela13076@users.noreply.github.com>
2023-02-26 16:23:17 -0600

    first commit

---
+++ github/timetable/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Calendar from './components/calendar';
+
+const App = () => {
+
+  return (
+    <div className="App">
+
+      <h1>Itinery for 7 Days in Colorado</h1>
+      <h2>Welcome to Colorado, Amelia! Check out this calendar to get to know the state and see all the sights during your stay.</h2>
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
---
+++ github/timetable/src/components/calendar.jsx
@@ -0,0 +1,128 @@
+import Event from "./Event";
+
+const Calendar = () => {
+    return (
+
+        <div className="Calender">
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Sunday</th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                    </tr>
+                </thead>    
+                <tbody>
+                    <tr>
+                        <td className="time">8 am</td>
+                        <Event event="Breakfast" color="pink" location="Denny's"/>
+                        <td></td>
+                        <td></td>
+                        <Event event="Go Running" color="blue" location="Wiggly Park"/>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">9 am</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">10 am</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">11 am</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">12 am</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">1 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">2 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">3 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <Event event="Visit Mountains" color="green" location="Mount Rushmore"/>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">4 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">5 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <Event event="Dinner" color="pink" location="Pizzaria"/>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    )
+}
+
+export default Calendar;
